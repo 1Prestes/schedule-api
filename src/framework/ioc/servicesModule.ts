66907131f021eb04257007a5460da43a67bd5edc
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2022-11-12 23:57:52 -0300

    fix: fix lint

--- src/framework/ioc/servicesModule.ts
+++ src/framework/ioc/servicesModule.ts
@@ -1,5 +1,5 @@
-import { ContainerModule, interfaces } from 'inversify'
+// import { ContainerModule, interfaces } from 'inversify'
 
-export const ServicesModule = new ContainerModule((bind: interfaces.Bind) => {
-  // bind<IService>(IServiceToken).to(Service)
-})
+// export const ServicesModule = new ContainerModule((bind: interfaces.Bind) => {
+//   bind<IService>(IServiceToken).to(Service)
+// })
