f1141d970dcbd6acfe2f0e9b03cff1ec17f3eb0b
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2022-11-12 22:10:22 -0300

    feat: add user migration

---
+++ devops/migrations/relational-database/20221112174038-User.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user', {
+      iduser: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(200),
+        allowNull: false,
+      },
+      birth_date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      username: {
+        type: Sequelize.STRING(50),
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('user')
+  }
+};
