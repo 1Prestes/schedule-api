08c9011b68e4da33bd442dc159e89e87b40de00a
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2022-11-15 03:19:15 -0300

    feat: add userContactEntity

---
+++ src/domain/entities/userContacts/userContactEntity.ts
@@ -0,0 +1,26 @@
+import { v4 as uuid } from 'uuid'
+
+import { AbstractEntity } from '../abstractEntity'
+import { Either, right } from '@shared/either'
+import { IError } from '@shared/iError'
+
+export interface IUserContactEntity {
+  id?: string
+  email?: string
+  phone?: string
+  default?: boolean
+  user_iduser?: string
+  createdAt?: Date
+}
+
+export class UserContactEntity extends AbstractEntity<IUserContactEntity> {
+  static create(props: IUserContactEntity): Either<IError, UserContactEntity> {
+    const user = new UserContactEntity({
+      ...props,
+      id: uuid(),
+      createdAt: new Date(),
+    })
+
+    return right(user)
+  }
+}
