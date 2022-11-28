96c9b817678ab18976ae4e4399aa6a3b3e404928
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2022-11-28 01:44:23 -0300

    feat: add authorizer

--- src/business/useCases/handle/handleJWT.ts
+++ src/business/useCases/handle/handleJWT.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 export class HandleJWT {
-  private readonly privateKey = process.env.SECRET_KEY
+  private readonly privateKey = process.env.JWT_SECRET
 
   public generateToken(iduser: string): string {
     return jwt.sign({ iduser }, this.privateKey, { expiresIn: 60 * 60 })
---
+++ src/framework/functions/authorizer/authorizer.ts
@@ -0,0 +1,53 @@
+const { verify } = require('jsonwebtoken')
+
+const validateToken = async token => {
+  const verified = verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      console.log('validateToken => ', err)
+      throw new Error('Unauthorized')
+    }
+
+    return decoded
+  })
+
+  return verified
+}
+
+const generatePolicy = (sub, allow, context = {}) => {
+  return {
+    principalId: sub || 'user-token',
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: allow ? 'Allow' : 'Deny',
+          Resource: '*',
+        },
+      ],
+    },
+    context: context,
+  }
+}
+
+module.exports.handler = async (event, context, callback) => {
+  const token = event?.authorizationToken
+  console.log('event :>> ', event)
+
+  if (!token) {
+    callback(null, generatePolicy(null, false))
+    return
+  }
+
+  const [, onlyToken] = token.split(' ')
+
+  try {
+    const verified = await validateToken(onlyToken)
+
+    const policy = generatePolicy(verified.iduser, true, verified)
+    callback(null, policy)
+  } catch (error) {
+    console.log('error => ', error)
+    callback(error)
+  }
+}
